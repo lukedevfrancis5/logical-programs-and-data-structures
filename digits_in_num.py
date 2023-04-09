a7abd45290b79db0555a7d88ef257d61e00e7e20
luke francis <lukedevfrancis@gmail.com>
2023-04-09 18:59:39 -0400

    made 2 programs that output reverse digits

--- digits_in_num.py
+++ digits_in_num.py
@@ -1,8 +1,15 @@
+# both programs output a reversed version of input
 
-nums = int(input("Enter some numbers: "))
+'''nums = int(input("Enter some numbers: "))
 
 while nums != 0:
     digits = nums % 10
     print(f"{digits}", end=" ")
     nums = nums // 10
+
+'''
+
+nums = input("Enter some numbers: ")
+for digit in nums[::1]:
+    print(f"{digit}", end=" ") 
     
